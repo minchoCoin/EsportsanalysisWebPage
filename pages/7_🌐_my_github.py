41475d2c547c2c3ac54e40289c459ac163992350
minchoCoin <62372650+minchoCoin@users.noreply.github.com>
2023-09-12 18:25:32 +0900

    Update 7_🌐_my_github.py

--- pages/7_🌐_my_github.py
+++ pages/7_🌐_my_github.py
@@ -6,10 +6,10 @@ def github():
     st.set_page_config(page_title="my_github", page_icon="🌐")
     st.markdown(
         '''
-        #### 아래 깃허브에서 ipynb파일로 더 정확하게 데이터 분석 결과를 볼 수 있습니다.
+        #### 아래 깃허브 repository에서 ipynb파일로 더 정확하게 데이터 분석 결과를 볼 수 있습니다.
          - https://github.com/minchoCoin/LOL_data_analysis_prj
 
-        #### 아래 깃허브에서 이 사이트가 어떤 방식으로 만들어졌는지 알 수 있습니다.
+        #### 아래 깃허브 repository에서 이 사이트가 어떤 방식으로 만들어졌는지 볼 수 있습니다.
          - https://github.com/minchoCoin/EsportsanalysisWebPage
         '''
     )
